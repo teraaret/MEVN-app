f413d0f5e7797c6aef70ecd5f16a91048b93c426
teraaret <raiiliiat@yandex.ru>
2019-01-19 00:08:08 +0500

    My hosting params

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    port: 8081,
+    host: '0.0.0.0',
+    port: 81,
     dbURL: 'mongodb://localhost/articles',
     dbOptions: {
-        useMongoClient: true
+//        useMongoClient: true,
+        useNewUrlParser: true
     }
 }
